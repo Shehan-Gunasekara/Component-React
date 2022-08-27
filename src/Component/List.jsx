f0dc9d2359662313232c4f0289e675914db0baf1
Shehan Gunasekara <87413135+it21011870@users.noreply.github.com>
2022-08-27 22:31:25 +0530

    Initial commit

---
+++ src/Component/List.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>BMW</li>
+      <li>Nissan petrol</li>
+      <li>Lamboghini</li>
+    </ul>
+  );
+}
+export default List;
